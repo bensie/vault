25b58dca593b6883e0572f33f0ecbe0c9b0d45e1
James Miller <bensie@gmail.com>
2014-08-06 21:40:19 -0700

    Improve route to config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :apps, only: [:show] do
-    resources :config_files, only: [:show]
+    resources :config_files, path: "", only: [:show]
   end
 
 end
