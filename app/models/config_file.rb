b25f515aa2aac37f096898d23a41ffda7c5a7b8d
James Miller <bensie@gmail.com>
2014-09-01 14:37:00 -0700

    Copy entire config files

--- app/models/config_file.rb
+++ app/models/config_file.rb
@@ -5,7 +5,7 @@ class ConfigFile < ActiveRecord::Base
 
   has_many :config_vars, dependent: :destroy
 
-  validates_presence_of :name
+  validates_presence_of :name, :app
 
   friendly_id :name
 
@@ -21,6 +21,13 @@ class ConfigFile < ActiveRecord::Base
       hash[app.environment]
     else
       hash
+    end
+  end
+
+  def copy_config_vars!(existing_config_file)
+    save!
+    existing_config_file.config_vars.each do |var|
+      config_vars.where(key: var.key).first_or_create(value: var.value)
     end
   end
 
